6ec4d5858770eca554cf1e70d472393af08bd19e
Jordan Walker <jwalker117@gmail.com>
2020-06-30 09:55:39 -0500

    added logo and ImageLinkForm components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 
 //Import components to be used on this page
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 
 class App extends Component {
   render() {
@@ -10,9 +13,15 @@ class App extends Component {
       <div className="App">
         <h1>Face Recognition Brain</h1>
         <Navigation />
-        {/* <Logo />
+        <Logo />
+        <Rank />
         <ImageLinkForm />
-        <FaceRecognition /> */}
+        {/*
+        <FaceRecognition /> 
+        */}
+        <footer>
+        Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+        </footer>
       </div>
     );
   }
